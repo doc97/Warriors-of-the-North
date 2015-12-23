e0ad33f23669cc2651ffb749d8ae837c9f31935e
doc97 <daniel.riissanen@gmail.com>
2015-12-23 13:22:10 +0200

    Initial commit with gradle setup

---
+++ android/src/com/tint/wotn/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tint.wotn.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tint.wotn.WarriorsOfTheNorth;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WarriorsOfTheNorth(), config);
+	}
+}
---
+++ desktop/src/com/tint/wotn/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tint.wotn.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tint.wotn.WarriorsOfTheNorth;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WarriorsOfTheNorth(), config);
+	}
+}
